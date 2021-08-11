fbf816cde88ba2eababaaf2f1a4c805867b8a935
josesales <salesbass@gmail.com>
2021-08-11 13:33:23 +0200

    deleting dist generated folder

--- server/dist/src/graphql/category/categorySchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var categorySchema = {
-    types: "#graphql\n        type Category {\n                id: String\n                name: String  \n                products: [Product]  \n            }\n    ",
-    inputs: "#graphql\n        input CategoryInput {\n                id: String\n                name: String\n            }\n    ",
-    queries: "#graphql\n        categories: [Category!]!",
-    mutations: "",
-};
-exports.default = categorySchema;
--- server/dist/src/graphql/checkout/checkoutSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var productSchema = {
-    types: "#graphql\n\n        type Checkout {\n            tokenId: String!\n            amount: Float!\n        }\n    ",
-    inputs: "#graphql\n        input CheckoutInput {\n            tokenId: String!\n            amount: Float!\n        }\n    ",
-    queries: "#graphql\n    ",
-    mutations: "#graphql\n        checkout(checkoutInput: CheckoutInput!): Boolean\n    ",
-};
-exports.default = productSchema;
